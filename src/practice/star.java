233cc538d9d7d5f1551950e06b1aaa54b5a85ef4
dongwoo2 <ieiedong@naver.com>
2023-04-23 21:36:56 +0900

    20230423 costudy

---
+++ src/practice/star.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class star {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for(int i = 0; i <x; i++) {
+            System.out.println("*");
+        }
+    }
+}
