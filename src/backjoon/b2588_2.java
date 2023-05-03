796605b5b8ea87a9dcf62d20251a577d5c596f10
dongwoo2 <ieiedong@naver.com>
2023-05-03 21:18:39 +0900

    20230503 cotest

---
+++ src/backjoon/b2588_2.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b2588_2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(A * (B % 10));
+        sb.append('\n');
+
+        sb.append(A * (B % 100)/10);
+        sb.append('\n');
+
+        sb.append(A * (B / 100));
+        sb.append('\n');
+
+        sb.append(A * B);
+
+        System.out.println("sb = " + sb);
+    }
+}
