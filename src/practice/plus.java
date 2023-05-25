9b4ea98b902c04fec0730b434270e838dd14918b
dongwoo2 <ieiedong@naver.com>
2023-05-25 22:11:07 +0900

    20230525 gittwo

--- src/practice/plus.java
+++ src/practice/plus.java
@@ -2,6 +2,7 @@ package practice;
 
 import java.util.Scanner;
 
+
 public class plus {
 
     public static void main(String[] args) {
@@ -9,13 +10,21 @@ public class plus {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-//        int k = sc.nextInt();
+        int k = sc.nextInt();
+        pl(n,k);
+        System.out.println(pl(n,k));
+//
+//        int sum = 0;
+//
+//        for(int i = 0 ; i <= n; i++) {
+//            sum = sum + i;
+//        }
+//        System.out.println("sum = " + sum);
+    }
 
-        int sum = 0;
-
-        for(int i = 0 ; i <= n; i++) {
-            sum = sum + i;
-        }
-        System.out.println("sum = " + sum);
+    public static int pl(int x,int y) {
+       int t = x+y;
+       int v = x-y;
+        return t + v;
     }
 }
