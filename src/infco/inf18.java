d090074f8383e68ffd5475cb704808904ac82926
dongwoo2 <ieiedong@naver.com>
2023-07-31 19:25:59 +0900

    20230731 inf18Cotest

--- src/infco/inf18.java
+++ src/infco/inf18.java
@@ -1,38 +1,19 @@
 package infco;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class inf18 {
-    public boolean isPrime(int num){
-        if(num == 1) {
-            return false;
-        }
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    return false;
-            }
-        }
-        return true;
-
-    }
-
-    public ArrayList<Integer> solution(int n , int[] arr){
-        ArrayList<Integer> answer = new ArrayList<>();
+    public int solution(int n, int[] arr) {
+        int cnt = 0;
+        int answer = 0;
         for (int i = 0; i < n; i++) {
-            int tmp = arr[i];
-            int res = 0;
-            while(tmp > 0){
-                int t = tmp % 10;
-                res = res * 10 + t;
-                tmp = tmp / 10;
+            if (arr[i] == 0) {
+                cnt = 0;
+            } else {
+                cnt++;
+                answer += cnt;
             }
-            if (isPrime(res)) {
-                answer.add(res);
-            }
-
         }
-
         return answer;
     }
 
@@ -41,11 +22,9 @@ public class inf18 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for(int x : T.solution(n , arr)){
-            System.out.println(x + " ");
-        }
+        System.out.print(T.solution(n,arr));
     }
 }
