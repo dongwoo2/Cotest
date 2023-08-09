a1def2ff5313d5d24e5f2494ae6a120b9b4050e5
dongwoo2 <ieiedong@naver.com>
2023-08-09 21:42:38 +0900

    20230809 javastudy 함수형인터에피읏

---
+++ src/javastudy/ch04/TestStringConcat.java
@@ -0,0 +1,12 @@
+package javastudy.ch04;
+
+public class TestStringConcat {
+
+    public static void main(String[] args) {
+
+        String s1 = "hello";
+        String s2 = "world";
+        StringConcatImpl.concat1 = new StringConcatImpl();
+        concat1.makeString(s1, s2);
+    }
+}
