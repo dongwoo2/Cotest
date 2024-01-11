5a2a48ea304871e6caa59a02f14d977bfdac6001
dongwoo2 <ieiedong@naver.com>
2024-01-11 20:27:56 +0900

    20240111 문제 테스트 필요 기능작업완료

--- src/teacherstudyre/Study0107_1.java
+++ src/teacherstudyre/Study0107_1.java
@@ -11,19 +11,24 @@ public class Study0107_1 {
     }
 
     public void solution() {
-        int sequence = 0; // 손님 순서
-        int allseque = 0;
+        int sequence = 1; // 손님 순서
+        int allseque = 1;
         int ch1 = 0;
         int foodmenu = 3;
+        int pcount = 0;
+        int allpcount = 0;
         int f1 = 0, f2 = 0, f3 = 0;
         int fprice[] = new int[foodmenu]; // 푸드메뉴의 가격
         int arr[] = new int[foodmenu+1]; // 돈과 음식갯수 담는 배열
         int se1 = 0; // 이전 된 사람인지 체크
+        int se2 = 0;
         int money = 0;
         int month = 1;
         int day = 1;
         int point = 0;
         int k = 0;
+        int paymoney = 0;
+        int ch4 = 0;
         int gubun = 0;
         boolean flag = true;
         boolean flag2 = true;
@@ -35,15 +40,26 @@ public class Study0107_1 {
         while (flag2 != false) {
 
             System.out.println(month + "월 " + day + "일");
-            sequence = 0;
+            System.out.println("손님은 몇 명 입니까?");
+            pcount = sc.nextInt();
+            allpcount += pcount;
+            sequence = 1;
+            if(se1 > 0) {
+                pcount += se1;
+            }
             flag = true;
+
+
             while (flag != false) {
+
                 if(gubun == 0) {
-                    System.out.println(sequence + 1 + "번 째 손님이 가진 돈은 얼마입니까?");
+                    System.out.println(sequence + "번 째 손님이 가진 돈은 얼마입니까?");
                     arr[0] = sc.nextInt(); //arr[0]은 money
                     money = arr[0]; // 손님이 처음 가지고 있던 돈
                     gubun++;
+                    point = 0;
                 }
+                ch4 = 0;
                 menu();
                 ch1 = sc.nextInt();
                 if(ch1 <= 3) {
@@ -51,55 +67,86 @@ public class Study0107_1 {
                     f1 += arr[1]; // bacon 이 부분 나중에 함수로
                     f2 += arr[2]; // icecream
                     f3 += arr[3]; // gamzacong
-//                    if(sequence != 1) k = sequence * 50;
-                    if(sequence == 0) {
-                        k = 0;
-                    } else {
-                        k = (5 * allseque);
-                    }
-                    for(int i = k; i < k + (allseque*5); i+=5) {
-                        people[i] = money;
-                        people[i+1] = f1;
-                        people[i+2] = f2;
-                        people[i+3] = f3;
-                        people[i+4] = point;
-                    }
+
+
                 } else if(ch1 == 4 || ch1 == 5) { //4번 다음사람
-                    if(f2 % 10 != 0) {
+
+                    if(f2 % 10 != 0 && arr[0] > 3000) {
                         icecream(f2);
+                        ch4++;
                     }
-                    if(f2 % 10 == 0 && f3 < f2) {
+                    if(f3 < f2 && arr[0] > 1000) {
                         gamza(f2,f3);
+                        ch4++;
                     }
-                    else if(ch1 == 5) {
+                    if(sequence == pcount && ch1 == 4) {
+                        System.out.println("no person please choose other choice");
+                        ch4++;
+                    }
+
+                    if(ch1 == 5 && ch4 == 0) {
                         flag = false;
                         flag2 = false;
+                        se2++;
+                    } else if(ch4 == 0 && ch1 == 4) {
+                        people = insertpeople(f1, f2, f3, money, point, se1, allseque, people,arr);
+
+
+                        f1 = 0;
+                        f2 = 0;
+                        f3 = 0;
+                        gubun = 0;
+                        sequence++;
+                        allseque++;
+                        daypeople[day]++;
+
+                        if(se1 != 0) {
+                            se1--;
+                            if(se1 < 0) {
+                                se1 = 0;
+                            }
+                        }
+
                     }
 
-                    gubun = 0;
-                    sequence++;
-                    allseque ++;
+
 
                 } else if(ch1 == 6) {
-
+                    paymoney = f1*fprice[0] + f2*fprice[1] + f3*fprice[2];
+                    if(paymoney >= 100000) {
+                        point = point(paymoney);
+                        System.out.println("포인트가" + point + "원 만큼 적립되었습니다.");
+                    }else {
+                        System.out.println("지불한 금액이 부족하여 포인트 적립이 불가합니다");
+                    }
                 } else if(ch1 == 7) { // 다음날
+                    people = insertpeople(f1, f2, f3, money, point, se1, allseque, people,arr);
+                    if(allpcount > allseque) {
+                        se1 = allpcount - allseque;
+                        System.out.println(se1 + "명 이전되었습니다.");
+                    }
 
-
-
-                    daypeople[day] = sequence;
+                    allseque++;
+                    daypeople[day]++;
                     day++;
                     if(day > 30) {
                         month++;
                         day = 1;
                     }
                     flag = false;
-
+                    gubun = 0;
+                    f1 = 0;
+                    f2 = 0;
+                    f3 = 0;
                 } else if(ch1 == 8) {
 //            	   receipt(people, arr, money, point, fprice);
-                    receipt(people, fprice, k, month, day);
+                    receipt(people, fprice, k, month, day, daypeople);
                 }
             }
 
+        }
+        if(se2 != 0) {
+            last(people, fprice, k, month, day, daypeople);
         }
     }
 
@@ -110,24 +157,43 @@ public class Study0107_1 {
 
     public int []foodselect(int choice, int money, int se1, int[]fPrice) {
         int f1 = 0, f2 = 0, f3 = 0;
-
-        if(se1 == 1) { // 할인
+        int fprice[] = new int [3];
+        if(se1 != 0) { // 할인
             int f1Discount = fPrice[0]/100;
             int f2Discount = fPrice[1]/100;
             int f3Discount = fPrice[2]/100;
-            fPrice[0] = fPrice[0] - (f1Discount * 5);
-            fPrice[1] = fPrice[1] - (f2Discount * 5);
-            fPrice[2] = fPrice[2] - (f3Discount * 5);
+            fprice[0] = fPrice[0] - (f1Discount * 5);
+            fprice[1] = fPrice[1] - (f2Discount * 5);
+            fprice[2] = fPrice[2] - (f3Discount * 5);
+        } else {
+            fprice[0] = fPrice[0];
+            fprice[1] = fPrice[1];
+            fprice[2] = fPrice[2];
         }
         if(choice == 1) { // 베이컨
-            money -= fPrice[0];
-            f1++;
+            if(money >= fprice[0]){
+                money -= fprice[0];
+                f1++;
+            } else{
+                System.out.println("돈이 부족합니다");
+                return new int[]{money,0,0,0};
+            }
         } else if (choice == 2) { // 아이스크림
-            money -= fPrice[1];
-            f2++;
+            if(money >= fprice[1]){
+                money -= fprice[1];
+                f2++;
+            } else{
+                System.out.println("돈이 부족합니다");
+                return new int[]{money,0,0,0};
+            }
         } else if (choice == 3) { // 감자콩
-            money -= fPrice[2];
-            f3++;
+            if(money >= fprice[2]){
+                money -= fprice[2];
+                f3++;
+            } else{
+                System.out.println("돈이 부족합니다");
+                return new int[]{money,0,0,0};
+            }
         }
         return new int[]{money,f1,f2,f3};
     }
@@ -159,9 +225,34 @@ public class Study0107_1 {
         return count;
     }
 
-    public void receipt(int people[], int fPrice[], int k, int month, int day) {
-        System.out.println("몇월 몇일자를 조회하시겠습니까?");
+    public int[] insertpeople(int f1, int f2, int f3, int money, int point, int se1 , int allseque ,int []people,int[]arr) {
+        int k = 0;
 
+        if(allseque == 1) {
+            k = 0;
+        } else {
+            k = 7 * (allseque-1);
+        }
+
+        for(int i = k; i < k + 7; i+=7) {
+            people[i] = money;
+            people[i+1] = f1;
+            people[i+2] = f2;
+            people[i+3] = f3;
+            people[i+4] = point;
+            people[i+5] = se1;
+            people[i+6] = arr[0];
+        }
+        return people;
+
+    }
+
+    public void receipt(int people[], int fPrice[], int k, int month, int day, int daypeople[]) {
+        System.out.println("몇월 몇일자를 조회하시겠습니까?");
+        int firstday = 0;
+        int lastday = 0;
+        int foodsize=fPrice.length+1;
+        int[]foodcnt = new int[foodsize];
         for(int t = 1, b = 1; t <= day && b <= month; t++) {
             System.out.print(t + " : " + b + "월 " + t + "일  ");
             if(t > 30) {
@@ -172,35 +263,90 @@ public class Study0107_1 {
         k = sc.nextInt();
 
         if(k != 1){
-            k = k * 50;
-        } else if(k == 1) {
-            k = 0;
+            for(int i = k; i >= 2; i--) {
+                firstday += daypeople[i-1]; //사람 수 시작 체크 3일차를 골랐다면 1,2 일차에 사람이 1일차 4명 2일차 3명 그러면 first에 사람수를 더한다
+                // 체크하는 인덱스 처음 시작용 변수
+            }
+            for(int j = k; j >= 1; j--) { //
+                lastday += daypeople[j];
+            }
         }
-        for(int i = k , psun = 1; i < k + 50; i+=5, psun++) {
-//        	people[k] = money;
-//        	people[k+1] = f1;
-//        	people[k+2] = f2;
-//        	people[k+3] = f3;
-//        	people[k+4] = point;
-            if(people[i] != 0 || people[i+1] != 0 || people[i+2] != 0 || people[i+3] != 0 ){
+        else {
+            firstday = 0;
+            lastday = daypeople[1];
+        }
+
+        firstday = (firstday*7);
+        lastday = (lastday*7)-1;
+        for(int i = firstday; i < lastday; i += 7) {
+            foodcnt[1] += people[i+1];
+            foodcnt[2] += people[i+2];
+            foodcnt[3] += people[i+3];
+        }
+        for(int i = firstday, psun = 1; i < lastday; i += 7,psun++) {
+            if(people[i] != 0 || people[i+1] != 0 || people[i+2] != 0 || people[i+3] != 0 || people[i+4] != 0 || people[i+5] != 0){
+                System.out.println("["+month + "월" + k + "일"+"]");
+                if(foodcnt[1] > 0)
+                    System.out.println("베이컨 x " + foodcnt[1] + " = " + fPrice[0]*foodcnt[1]);
+                if(foodcnt[2] > 0)
+                    System.out.println("아이스크림 x " + foodcnt[2] + " = " + fPrice[1]*foodcnt[2]);
+                if(foodcnt[3] > 0)
+                    System.out.println("감자콩 x " + foodcnt[3] + " = " + fPrice[2]*foodcnt[3]);
+                System.out.println("================================================================");
                 System.out.println(psun + "번 째 사람");
+                if(people[i+5] != 0) {
+                    System.out.println("할인 대상입니다!");
+                }
                 System.out.println("원래가진돈 : "+people[i]);
                 System.out.println("베이컨 : "+people[i+1]);
                 System.out.println("아이스 : "+people[i+2]);
                 System.out.println("감자콩 : "+people[i+3]);
+                if(people[i+4] > 0)
+                    System.out.println("포인트 : "+people[i+4]);
+                System.out.println("남은금액 : " + people[i+6]);
                 System.out.println("================================================================");
             }
         }
-//    	int allbacon = fPrice[0] * arr[1];
-//    	int allice = fPrice[1] * arr[2];
-//    	int allpotato = fPrice[2] * arr[3];
-//    	int allprice = allbacon + allice + allpotato;
-//    	int minusmoney = money - allprice;
-//    	System.out.println("originally money : " + money + "won" );
-//    	System.out.println("Bacon : " + allbacon + "won");
-//    	System.out.println("ice : " + allice + "won");
-//    	System.out.println("potato : " + allpotato + "won");
-//    	System.out.println("now money : " + minusmoney + "won");
+
+
+    }
+
+    public void last(int people[], int fPrice[], int k, int month, int day, int daypeople[]) {
+        int firstday = 0;
+        int lastday = 0;
+        int x = 0;
+        for(int t = 1, b = 1; t <= day && b <= month; t++) {
+            System.out.println(b + "월 " + t + "일  :" + daypeople[t] +"명" );
+
+            if(t != 1){
+                for(int i = t; i >= 2; i--) {
+                    firstday += daypeople[i-1]; //사람 수 시작 체크 3일차를 골랐다면 1,2 일차에 사람이 1일차 4명 2일차 3명 그러면 first에 사람수를 더한다
+                    // 체크하는 인덱스 처음 시작용 변수
+                }
+                for(int j = t; j >= 1; j--) { //
+                    lastday += daypeople[j];
+                }
+            }
+            else {
+                firstday = 0;
+                lastday = daypeople[1];
+            }
+
+            firstday = (firstday*7);
+            lastday = (lastday*7)-1;
+            for(int i = firstday, psun = 1; i < lastday; i+=7, psun++) {
+                if(people[i+4] > 0) {
+                    System.out.println(psun + "번 째 사람이 포인트를 " + people[i+4]+ " 만 큼 가지고 있습니다.");
+                    x += people[i+4];
+                }
+            }
+            System.out.println(b + "월" + t + "일 포인트 총 합계는" + x + "원 입니다." );
+            x = 0;
+            if(t > 30) {
+                t = 1;
+                b++;
+            }
+        }
     }
 
 
