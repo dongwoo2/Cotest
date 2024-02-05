3e84e8240e935f669bec1dd3e221ce40c31f7af8
dongwoo2 <ieiedong@naver.com>
2024-02-05 19:57:23 +0900

    20240205 개미문제 풀이시작

---
+++ src/teacherstudyre/Study20240205_1.java
@@ -0,0 +1,13 @@
+package teacherstudyre;
+
+public class Study20240205_1 {
+
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static void solution() {
+        int line[] = new int[24]; 
+    }
+
+}
