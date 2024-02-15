b31f3650ad7cdb85f861bdc23a9923b9afae70cb
dongwoo2 <ieiedong@naver.com>
2024-02-16 00:52:05 +0900

    20240216 노트북에서 확인용

--- src/teacherstudyre/Study20240205_1_2.java
+++ src/teacherstudyre/Study20240205_1_2.java
@@ -7,20 +7,30 @@ public class Study20240205_1_2 {
     public static void main(String[] args) {
         Random random = new Random();
         int [] ants = new int[4];
+        int [] touchs = new int[4];
+        ants[0] = 100; // 오류방지
+
 
         // 개미 초기 위치 설정
-        ants[1] = random.nextInt();
+        ants[3] = random.nextInt(24);
+        ants[2] = ants[3] - random.nextInt(ants[3]);
+        while(ants[2] == ants[3]) {
+            ants[2] = ants[3] - random.nextInt(ants[3]);
+        }
+        ants[1] = ants[2] - random.nextInt(ants[2]);
+        while(ants[1] == ants[2]) {
+            ants[1] = ants[2] - random.nextInt(ants[2]);
+        }
+        System.out.println("ants1 = " + ants[1]);
+        System.out.println("ants2 = " + ants[2]);
+        System.out.println("ants3 = " + ants[3]);
         int minute = 0;
         boolean flag = true;
         while (flag) {
             minute++;
-            for(int i = 1; i<=3; i++) {
-                ants[i]++;
-                if(ants[i] < ants[i-1] && ants[i] < ants[i+1]) {
-
-                }
-            }
+            if(t)
         }
     }
 }
+
 
