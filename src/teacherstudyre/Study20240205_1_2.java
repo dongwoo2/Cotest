d52faaf9b4861f3ac231847acbd7a435d79c1497
dongwoo2 <ieiedong@naver.com>
2024-02-20 21:39:13 +0900

    자바 개미문제 풀이 1번개미가 2번개미 넘어가는 상황 발생 해결 필요

--- src/teacherstudyre/Study20240205_1_2.java
+++ src/teacherstudyre/Study20240205_1_2.java
@@ -8,7 +8,9 @@ public class Study20240205_1_2 {
         Random random = new Random();
         int[] ants = new int[4];
         int[] touchs = new int[4];
-        int[] ranking = new int[3];
+        int[] ranking = new int[4];
+        int[] rankingtime = new int[4];
+        int rankingcheck = 0;
         int touch = 0;
         ants[0] = 100; // 오류방지
 
@@ -48,18 +50,17 @@ public class Study20240205_1_2 {
                     }
 
                     } else { // 개미가 움직이는 방향도 랜덤으로 해볼까? 정해놓지 말고?
-                    if(i == 1 && touchs[i] == 0) {
+                    if(i == 1 && touchs[i] == 0) { //1번 개미
                         ants[i]++;
                     } else if(i == 1 && touchs[i] != 0) {
                         ants[i]--;
                     }
-
-                    if (i == 2 && touchs[i] == 0) {
+                    if (i == 2 && touchs[i] == 0) { //2번 개미
                         ants[i]--;
                     } else if(i == 2 && touchs[i] != 0) {
                         ants[i]++;
                     }
-                    if (i == 3 && touchs[i] == 0) {
+                    if (i == 3 && touchs[i] == 0) { //3번 개미
                         ants[i]++;
                     } else if(i == 3 && touchs[i] != 0) {
                         ants[i]--;
@@ -67,15 +68,32 @@ public class Study20240205_1_2 {
                 }
 
 
-                if(ants[i] < 0 || ants[i] > 24) {
-                    if(ranking[1] == 0) {
-                        
+                if(ants[i] == 0 || (ants[i] > 23 && ants[i] < 100)) {
+                    ants[i] = 101;
+                    rankingcheck++;
+                    if(rankingcheck == 1) {
+                        ranking[1] = i;
+                        rankingtime[1] = minute;
                     }
-
+                    if(rankingcheck == 2) {
+                        ranking[2] = i;
+                        rankingtime[2] = minute;
+                    }
+                    if(rankingcheck == 3) {
+                        ranking[3] = i;
+                        rankingtime[3] = minute;
+                    }
+                }
+                if(rankingcheck == 3) {
+                    flag = false;
                 }
 
+                System.out.println("ants[" +i+"] : " + ants[i]);
             }
         }
+        System.out.println("1등 : " +  ranking[1] + "번 개미\n도착시간 : " + rankingtime[1]);
+        System.out.println("2등 : " +  ranking[2] + "번 개미\n도착시간 : " + rankingtime[2]);
+        System.out.println("3등 : " +  ranking[3] + "번 개미\n도착시간 : " + rankingtime[3]);
     }
 }
 
