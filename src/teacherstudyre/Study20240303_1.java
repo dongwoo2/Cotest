b2b8ea966076329489088be7a132e921fec03724
dongwoo2 <ieiedong@naver.com>
2024-03-04 22:26:52 +0900

    20240304 숙제 제거 부분 완료

--- src/teacherstudyre/Study20240303_1.java
+++ src/teacherstudyre/Study20240303_1.java
@@ -13,24 +13,35 @@ public class Study20240303_1 {
     public static void solution() {
         int n = random.nextInt(50) + 50;
         int ch = random.nextInt(3) + 1;
-        int k = n/3;
+        int k = n/3; // 홀수 일 경우 짝수 일 경우 생각해야함
         int t = 0;
+        int removecnt = 0;
         int cardcnt[] = new int[n];
         for(int i = 0; i < cardcnt.length; i++) {
             cardcnt[i] = random.nextInt(9999) + 1;
             System.out.println("i = " + i);
             System.out.println(cardcnt[i]);
         }
-        if(ch == 1) { // 3으로 나눠서 앞 부분만
-            //while (k % 4 != 0 || k % 5 != 0 || k % 6 != 0) {
-            //    k++;
-            //}
-            t = random.nextInt(k);
-            t = sc.nextInt();
-            if(t > k) { // 크면 안됨 앞 부분이니까
+
+        cardcnt = removecard(cardcnt, removecnt, k);
+
+        int cardcnt2[] = new int[k];
+        for(int j = 0; j < cardcnt.length; j++) {
+            if (cardcnt[j] != 0) {
 
             }
-            while(cardcnt[t] < 100 && cardcnt[t] > 8000 && (cardcnt[t] % 4 != 0 || cardcnt[t] % 5 != 0 || cardcnt[t] != 0)) {
+        }
+
+        if(ch == 1) { // 3으로 나눠서 앞 부분만
+            System.out.println("앞 쪽 3분의 1부분만 남았습니다.");
+            System.out.println("몇 번째 카드를 고르시겠습니까?");
+            t = sc.nextInt();
+            if(t > k) { // 크면 안됨 앞 부분이니까
+                System.out.println("잘못된 카드를 고르셨습니다. ");
+            } else {
+
+            }
+            while(cardcnt[t] < 100 && cardcnt[t] > 8000 && (cardcnt[t] % 4 != 0 || cardcnt[t] % 5 != 0 || cardcnt[t] % 6 != 0)) {
                t = random.nextInt(k);
             }
         } else if (ch == 2) { // 얘는 중간 부분만
@@ -39,12 +50,12 @@ public class Study20240303_1 {
 
             }
             t = random.nextInt(k) + k; // 그리고 앞 부분 빼기 다시 k로 빼기
-            while(cardcnt[t] < 100 && cardcnt[t] > 8000 && (cardcnt[t] % 4 != 0 || cardcnt[t] % 5 != 0 || cardcnt[t] != 0)) {
+            while(cardcnt[t] < 100 && cardcnt[t] > 8000 && (cardcnt[t] % 4 != 0 || cardcnt[t] % 5 != 0 || cardcnt[t] % 6 != 0)) {
                 t = random.nextInt(k) + k;
             }
         } else { // 얘는 끝 부분만
             t = random.nextInt(k) + k * 2;
-            while(cardcnt[t] < 100 && cardcnt[t] > 8000 && (cardcnt[t] % 4 != 0 || cardcnt[t] % 5 != 0 || cardcnt[t] != 0)) {
+            while(cardcnt[t] < 100 && cardcnt[t] > 8000 && (cardcnt[t] % 4 != 0 || cardcnt[t] % 5 != 0 || cardcnt[t] % 6 != 0)) {
                 t = random.nextInt(k) + k * 2;
             }
             // 앞에 두 부분 빼면됨
@@ -52,5 +63,22 @@ public class Study20240303_1 {
 
 
 
+
     }
+
+    public static int[] removecard(int cardcnt[], int removecnt, int k) {
+        for(int j = 0; j < cardcnt.length; j++) { //카드제거
+            if(cardcnt[j] < 100 && cardcnt[j] > 8000 && (cardcnt[j] % 4 != 0 || cardcnt[j] % 5 != 0 || cardcnt[j] % 6 != 0) && removecnt < (k*2)) {
+                cardcnt[j] = 0;
+                removecnt++;
+            }
+        }
+        if(removecnt < k*2) { // 조건에 따라 제거한 카드가 제거되어야 할 양보다 작을 떄 추가로 제거
+            for(int q = 0; removecnt < k * 2; q++,removecnt++) {
+                cardcnt[q] = 0;
+            }
+        }
+        return cardcnt;
+    }
+
 }
