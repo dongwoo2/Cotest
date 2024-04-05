393fa32edbb3383bf051f4a2b006c177f8623cf2
dongwoo2 <ieiedong@naver.com>
2024-04-05 23:26:26 +0900

    20240405 수업 층 바뀔때 오류 잡아야함

--- src/teacherstudyre/Study20240322_2.java
+++ src/teacherstudyre/Study20240322_2.java
@@ -70,8 +70,10 @@ public class Study20240322_2 {
 
     public static int[][] moveinage(int[][] apartage, int choice, int choice1, int movein_age, int choho, int choho1) {
         int length = 0;
-        int cnt = 0;
+        int cnt = movein_age;
         int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
         int plus = 0;
         int ho = choice1;
         boolean flag = true;
@@ -91,7 +93,7 @@ public class Study20240322_2 {
                     cnt = apartage[choice][choice1]; // 원래 층
                     apartage[choice][choice1] = movein_age;
 
-                    if(apartage[choice].length != ho) { // 층 안바꿔도 될 떄
+                    if(apartage[choice].length >= ho) { // 층 안바꿔도 될 떄
                         apartage[choice][choice1 + 1] = cnt;
                     } else { // 층 바꿔야 할 때
                         choice++;
@@ -107,7 +109,7 @@ public class Study20240322_2 {
                     }
 
                 } else { //2번째
-                     if(apartage[choice].length != ho) {
+                     if(apartage[choice].length >= ho) {
                          choice1++;
                          apartage[choice][choice1] = apartage[choice][choice1-1];
                      } else {
@@ -126,43 +128,59 @@ public class Study20240322_2 {
             }
         } else { // 같거나 작으면 왼쪽으로
             while (flag) {
-                plus++;
-                ho--; // 층이 바뀌어버리면 호가 다시 커져야한다
-                if (plus == 1) { //바꾸는중에 넣어야할 거 같은데 층 바꾸는 걸
-                    cnt = apartage[choice][choice1]; // 원래 층
-                    apartage[choice][choice1] = movein_age;
+                    plus++;
+                    ho--;
+                    if(ho == 0) {
+                        ho = apartage[choice].length;
+                    }
 
-                    if(apartage[choice].length != ho) { // 층 안바꿔도 될 떄
-                        apartage[choice][choice1 - 1] = cnt;
-                    } else { // 층 바꿔야 할 때
+                    if(choice1 < 0 && choice >= 1 && cnt2 > 0) { // 층을 바꾸어야 할 때
                         choice--;
-                        if(choice >= 0) { //층이 제일 낮은 층보다 낮아지면 안됨
-                            cnt1 = choice1;
-                            choice1 = 0;
-                            apartage[choice][choice1] = apartage[choice+1][cnt1];
-                            ho = apartage[choice].length;
+                        choice1 = 0;
+                        if(plus == 1)  { //처음
+                            cnt = movein_age; // 상대를 바뀌게 해주어야하는 값
+                            cnt1 = apartage[choice][choice1]; // 바뀐 사람이 가지고있는 원래값
+                            apartage[choice][choho1] = cnt;
+                            cnt = cnt1;
+                            choice1 = apartage[choice].length-1;
+                            cnt2 = 0;
+                        } else { // 두 번째
+                            if(plus > 1) {
+                                cnt = cnt1;
+                            } else {
+                                cnt = apartage[choice+1][choice1];
+                            }
+                            cnt1 = apartage[choice][choho1-1];
+                            apartage[choice][choho1-1] = cnt;
+                            cnt = cnt1;
+                            choice1 = apartage[choice].length-1;
+                            cnt2 = 0;
+                        }
+                    } else if(choice1 == 0 && choice == 0){ // 끝났을 때
+                        flag = false;
+                    } else if(choice1 >= 0){ // 층을 안바꾸고 진행가능할 때
+                        if(plus == 1) {
+                            cnt = movein_age;
+                            cnt1 = apartage[choice][choice1];
+                            apartage[choice][choice1] = cnt;
+                            choice1--;
+                            System.out.println("choice1 = " + choice1);
+                            System.out.println("apartage[choice] = " + apartage[choice].length);
+                            if(choice1 < 0) {
+                                cnt2++;
+                            }
                         } else {
-                            flag = false;
+                            cnt = apartage[choice][choice1];
+                            apartage[choice][choice1] = cnt1;
+                            cnt1 = cnt;
+                            choice1--;
+                            System.out.println("choice1 = " + choice1);
+                            if(choice1 < 0) {
+                                cnt2++;
+                            }
                         }
 
                     }
-
-                } else { //2번째
-                    if(apartage[choice].length != ho) {
-                        choice1--;
-                        apartage[choice][choice1] = apartage[choice][choice1+1];
-                    } else {
-                        if(choice >= 0) {
-                            choice--;
-                            cnt1 = choice1;
-                            choice1 = apartage[0].length-1;
-                            apartage[choice][choice1] = apartage[choice-1][cnt1];
-                            ho = apartage[choice].length-1;
-                        } else {
-                            flag = false;
-                        }
-                    }
-                }
 
             }
         }
