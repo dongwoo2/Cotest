fbab7277768cbe82a121f2aabe71244b9b3f98e1
dongwoo2 <ieiedong@naver.com>
2024-11-06 20:12:44 +0900

    20241106 에너지 건물 구입 함수 로직 구현, 에너지 대출 구현중

--- src/teacherstudyre/Study20241031_1.java
+++ src/teacherstudyre/Study20241031_1.java
@@ -14,7 +14,7 @@ public class Study20241031_1 {
         boolean flag3 = true;
         int turn = 0;
         int choice = 0;
-        int building_choice = 0;
+
 
         // 존 마다 사람수도 채킹 해야하는데
         // 에너지 건물은 어떻게 하지? 클래스?
@@ -37,22 +37,9 @@ public class Study20241031_1 {
                 System.out.println("1. 에너지 건물 구입 2. 에너지 대출");
                 choice = sc.nextInt();
                 if(choice == 1) {
-                    while (flag3) { // TODO 함수로 바꾸기
-                        System.out.println("1. 빌딩 2. 호텔 3. 랜드마크");
-                        building_choice = sc.nextInt();
-                        if (villages[c].energy_building[0] == building_choice) {
-                            System.out.println("현재 똑같은 건물을 소유하고 있습니다.");
-                            System.out.println("1. 다시선택 2. 나가기");
-                            choice = sc.nextInt();
-                            if(choice == 2) {
-                                flag3 = false;
-                            } else if (choice == 1) {
+                    villages = equipmentBuilding(villages, c); // 건물 구입 함수
+                } else if(choice == 2) {
 
-                            } else {
-                                System.out.println("잘못 선택하셨습니다 건물 선택화면으로 돌아갑니다.");
-                            }
-                        }
-                    }
                 }
                 c++;
                 if(c > villages.length) {
@@ -116,6 +103,95 @@ public class Study20241031_1 {
                 System.out.println( vi[i].name +" 마을의 " + (j + 1) + "번 째 존의 에너지" + vi[i].zone[j]);
             }
         }
+    }
+
+    public static Village[] equipmentBuilding(Village[] vi, int c) {
+        int choice = 0;
+        int building_choice = 0;
+        int building_price = 0;
+        boolean flag = true;
+        boolean flag2 = true;
+        int check = 0;
+        int whileout = 0;
+        while (flag) { // 에너지빌딩[0] = 존[0] 즉 존에 있는 빌딩임
+            System.out.println("몇 번째 존을 고르시겠습니까?");
+            choice = sc.nextInt();
+            if(vi[c].zoneCount > choice || vi[c].zoneCount < choice) {
+                System.out.println("잘못 선택하셨습니다.");
+                System.out.println("zone 선택화면으로 돌아갑니다.");
+            } else {
+                while (flag2) {
+                    System.out.println("1. 빌딩 2. 호텔 3. 랜드마크"); // 가격 200,400,800
+                    building_choice = sc.nextInt();
+                    if (building_choice == 1) {
+                        building_price = 200;
+                        check = 1;
+                    } else if (building_choice == 2) {
+                        building_price = 400;
+                        check = 1;
+                    } else if (building_choice == 3) {
+                        building_price = 800;
+                        check = 1;
+                    } else {
+                        System.out.println("잘못된 번호를 선택하셨습니다.");
+                    }
+
+
+                    if (check == 1) {
+                        if (vi[c].energy_building[choice] == building_choice) { // 똑같은 건물을 선택했을 시
+                            System.out.println("현재 똑같은 건물을 소유하고 있습니다.");
+                            System.out.println("1. 다시선택 2. 나가기");
+                            choice = sc.nextInt();
+                            if (choice == 2) {
+                                flag = false;
+                                flag2 = false;
+                            } else if (choice == 1) {
+                                check = 0;
+                            } else {
+                                whileout = 1;
+                            }
+                        } else if (vi[c].energy_building[choice] > building_choice) {
+                            System.out.println("현재 선택한 건물보다 더 좋은 건물을 소유하고 있습니다.");
+                            System.out.println("1. 다시선택 2. 나가기");
+                            choice = sc.nextInt();
+                            if (choice == 2) {
+                                flag = false;
+                                flag2 = false;
+                            } else if (choice == 1) {
+                                check = 0;
+                            } else {
+                                whileout = 1;
+                            }
+                        } else {
+                            if (vi[c].zone[choice] > building_price) {
+                                System.out.println("구입이 완료되었습니다.");
+                                flag2 = false;
+                                flag = false;
+                            } else {
+                                System.out.println("금액이 모자랍니다. 선택화면으로 돌아갑니다.");
+                            }
+                        }
+                        if(whileout == 1) {
+                            System.out.println("잘못 선택하셨습니다 건물 선택화면으로 돌아갑니다.");
+                            whileout = 0;
+                        }
+                    }
+                }
+            }
+
+
+        }
+        return vi;
+    }
+
+    public static Village[] energyLoan(Village[] vi, int c) { // 대출
+        int choice = 0;
+        System.out.println("어느 존이 대출을 받겠습니까?");
+        choice = sc.nextInt();
+        //vi[c].zone[choice - 1]
+
+
+        return vi;
     }
 
 }
