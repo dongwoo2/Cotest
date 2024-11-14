57e9aa70de9e9bc055d3527d40c79583307177f6
dongwoo2 <ieiedong@naver.com>
2024-11-14 16:57:00 +0900

    20241114 최단거리 재작업 필요

---
+++ src/teacherstudyre/Study20241114_1.java
@@ -0,0 +1,14 @@
+package teacherstudyre;
+
+import java.util.Scanner;
+
+public class Study20241114_1 {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        char[][] map;
+        int rows;
+        int cols;
+        int[] start;
+        int[] destination;
+    }
+}
